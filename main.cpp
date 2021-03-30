5a2d832338f41fce52871ef463fba69e562b54b7
Daniel Rapetti <iximiel@gmail.com>
2021-03-30 10:38:02 +0200

    renaming and adding some more protecttion

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
-#include "simplexMinimizator.h"
+#include "simplexNelderMeadMinimizator.h"
 
 int main(int, char **) {
     constexpr int N=2;
-    simplexMethodMinimization::simplex<double,N>::vertex startingvertex;
+    simplexNelderMeadMethodMinimization::simplex<double,N>::vertex startingvertex;
     for(int i=0;i<N;++i) {
         startingvertex[i]=1.0;
     }
-    auto t = simplexMethodMinimization::simplexMinimizerFromStartingVertex<double,N>(2.5,startingvertex,[](double* x)->double{
+    auto t = simplexNelderMeadMethodMinimization::minimizerNelderMeadFromStartingVertex<double,N>(2.5,startingvertex,[](double* x)->double{
         //return (x[0]-1)*(x[0]-1)+x[1]*x[1];
         //return (x[0]-1)*(x[0]-1) + 100.0*(x[1]-x[0]*x[0])*(x[1]-x[0]*x[0]);
         double a=(x[0]*x[0]+x[1]-11);
--- simplexNelderMeadMinimizator.h
+++ simplexNelderMeadMinimizator.h
@@ -5,23 +5,27 @@
 #include <iostream>
 #include <functional>
 
-namespace simplexMethodMinimization {
+namespace simplexNelderMeadMethodMinimization {
 constexpr unsigned maxFunctionEvaluations = 5000;
 
-template <typename T, unsigned N> struct simplex {
+template <typename T, unsigned N>
+struct simplex {
   struct vertex {
     vertex() = default;
+
     vertex(const vertex &other) : value_(other.value_) {
       for (unsigned i = 0; i < N; ++i) {
         coordinates_[i] = other.coordinates_[i];
       }
     }
+
     vertex(vertex &&other)
         : value_(std::move(other.value_)), coordinates_(other.coordinates_) {}
+
     void evaluation(std::function<T (T *)> function) {
       value_ = function(coordinates_.data());
-      // std::cout << value <<std::endl;
     }
+
     vertex &operator=(const vertex &other) {
       if (this != &other) {
         for (unsigned i = 0; i < N; ++i) {
@@ -31,6 +35,7 @@ template <typename T, unsigned N> struct simplex {
       }
       return *this;
     }
+
     vertex &operator=(vertex &&other) {
       if (this != &other) {
         std::swap(coordinates_, other.coordinates_);
@@ -38,32 +43,42 @@ template <typename T, unsigned N> struct simplex {
       }
       return *this;
     }
+
     // sorting
     bool operator>(const vertex &other) { return value_ > other.value_; }
     bool operator<(const vertex &other) { return value_ < other.value_; }
+
+    //accessing
     T operator[](const unsigned &i) const { return coordinates_[i]; }
     T &operator[](const unsigned &i) { return coordinates_[i]; }
+
     T getValue() const { return value_; }
 
   protected:
     T value_{0.0};
     std::array<T, N> coordinates_;
   };
+
   simplex() = default;
+
   simplex(const simplex& s):vertices_(s.vertices_){}
+
   simplex(const simplex&& s):vertices_(s.vertices_){}
+
   simplex& operator=(const simplex& s){
       if(this!=&s) {
           vertices_=s.vertices_;
       }
       return *this;
   }
+
   simplex& operator=(const simplex&& s){
       if(this!=&s) {
           vertices_.swap(s.vertices_);
       }
       return *this;
   }
+
   simplex(T intitialDisplacement, const vertex &startingvertex) {
     vertices_[0] = startingvertex;
     for (unsigned i = 1; i < N + 1; ++i) {
@@ -71,7 +86,8 @@ template <typename T, unsigned N> struct simplex {
       vertices_[i][i - 1] += intitialDisplacement;
     }
   }
-  inline void centroidCalculation() {
+
+  inline void updateCentroid() {
     for (unsigned i = 0; i < N; ++i) {
       centroid_[i] = vertices_[0][i];
       // Numerical Recipes uses also the last point in the centroid, wikipedia
@@ -81,6 +97,11 @@ template <typename T, unsigned N> struct simplex {
       }
     }
   }
+
+  void orderVertices() {
+    std::sort(vertices_.begin(), vertices_.end());
+  }
+
   vertex extrapolateOnTheWorst(const T &factor, std::function<T (T *)> function) const {
     vertex newTry;
     T f1 = (1 - factor) / N;
@@ -91,13 +112,19 @@ template <typename T, unsigned N> struct simplex {
     newTry.evaluation(function);
     return newTry;
   }
+
+  //accessing
+  vertex operator[](const unsigned &i) const { return vertices_[i]; }
+  vertex &operator[](const unsigned &i) { return vertices_[i]; }
+
+  protected:
   std::array<vertex, N + 1> vertices_;
   std::array<T, N> centroid_;
 };
 
 template <typename T, unsigned dim>
 typename simplex<T, dim>::vertex
-simplexMinimizer(simplex<T, dim> s,
+minimizerNelderMead(simplex<T, dim> s,
                  std::function<T (T *)> function) {
   constexpr T notDen0 = 1e-10;
 
@@ -108,39 +135,39 @@ simplexMinimizer(simplex<T, dim> s,
   unsigned functionEvaluations = 0;
   while (functionEvaluations < maxFunctionEvaluations) {
     // order
-    std::sort(s.vertices_.begin(), s.vertices_.end());
-    s.centroidCalculation();
+    s.orderVertices();
+    s.updateCentroid();
     rtol = 2.0 *
-           std::abs(s.vertices_[dim].getValue() - s.vertices_[0].getValue()) /
-           (std::abs(s.vertices_[dim].getValue()) +
-            std::abs(s.vertices_[0].getValue()) + notDen0);
+           std::abs(s[dim].getValue() - s[0].getValue()) /
+           (std::abs(s[dim].getValue()) +
+            std::abs(s[0].getValue()) + notDen0);
     if (rtol < ftol) {
       break;
     }
     // reflection
     Vertex reflection = s.extrapolateOnTheWorst(-1.0, function);
     ++functionEvaluations;
-    if (reflection < s.vertices_[0]) { // expand if is a good move
-      s.vertices_[dim] = reflection;
+    if (reflection < s[0]) { // expand if is a good move
+      s[dim] = reflection;
       Vertex expansion = s.extrapolateOnTheWorst(2.0, function);
       ++functionEvaluations;
       if (expansion < reflection) {
-        s.vertices_[dim] = expansion;
+        s[dim] = expansion;
       }
-    } else if (reflection < s.vertices_[dim - 1]) { // reflection
-      s.vertices_[dim] = reflection;
+    } else if (reflection < s[dim - 1]) { // reflection
+      s[dim] = reflection;
     } else { // contraction or if contraction fails shrink
       Vertex contraction = s.extrapolateOnTheWorst(0.5, function);
       ++functionEvaluations;
-      if (contraction < s.vertices_[dim]) { // contraction
-        s.vertices_[dim] = contraction;
+      if (contraction < s[dim]) { // contraction
+        s[dim] = contraction;
       } else { // shrink
         for (unsigned j = 1; j < dim + 1; ++j) {
           for (unsigned i = 0; i < dim; ++i) {
-            s.vertices_[j][i] = 0.5 * (s.vertices_[j][i] - s.vertices_[0][i]);
+            s[j][i] = 0.5 * (s[j][i] - s[0][i]);
             // x0 + alpha *(xi-x0)
           }
-          s.vertices_[j].evaluation(function);
+          s[j].evaluation(function);
           ++functionEvaluations;
         }
       }
@@ -148,21 +175,21 @@ simplexMinimizer(simplex<T, dim> s,
   }
   std::cout << functionEvaluations << " / " << maxFunctionEvaluations
             << std::endl;
-  return s.vertices_[0];
+  return s[0];
 }
 
 template <typename T, unsigned dim>
 typename simplex<T, dim>::vertex
-simplexMinimizerFromStartingVertex(T intitialDisplacement,
+minimizerNelderMeadFromStartingVertex(T intitialDisplacement,
                  typename simplex<T, dim>::vertex startingvertex,
                  std::function<T (T *)> function) {
     using Simplex = simplex<T, dim>;
 
     Simplex s(intitialDisplacement, startingvertex);
     for (unsigned i = 0; i < dim + 1; ++i) {
-        s.vertices_[i].evaluation(function);
+        s[i].evaluation(function);
     }
-    return simplexMinimizer(s,function);
+    return minimizerNelderMead(s,function);
 }
 } // namespace simplexMethodMinimization
 #endif // SIMPLEXMINIMIZATOR_H
